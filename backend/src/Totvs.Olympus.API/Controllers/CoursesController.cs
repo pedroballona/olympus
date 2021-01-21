a0fecfe3adcb8c73099892d10f1b7ce49c184b0d
Sílvio Freitas <silvinhforeitas@live.com>
2021-01-21 15:31:53 -0300

    Implementação do MongoDB

--- backend/src/Totvs.Olympus.API/Controllers/CoursesController.cs
+++ backend/src/Totvs.Olympus.API/Controllers/CoursesController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Totvs.Olympus.CrossCutting.DTOs;
 using Totvs.Olympus.CrossCutting.ExternalServices.ResponsesDTO;
 using Totvs.Olympus.Domain.ExternalServices;
+using Totvs.Olympus.Domain.Services;
 
 namespace Totvs.Olympus.API.Controllers
 {
@@ -12,10 +14,13 @@ namespace Totvs.Olympus.API.Controllers
   public class CoursesController : ControllerBase
   {
     private readonly IGetAllCoursesFromAluraService _service;
+    private readonly ICourseService _courseService;
 
-    public CoursesController(IGetAllCoursesFromAluraService service)
+    public CoursesController(IGetAllCoursesFromAluraService service,
+                             ICourseService courseService)
     {
       _service = service;
+      _courseService = courseService;
     }
 
     [HttpGet]
@@ -23,6 +28,22 @@ namespace Totvs.Olympus.API.Controllers
     public async Task<IEnumerable<BasicCourseDataDTO>> GetAllCourses()
     {
       var result = await _service.GetCourses();
+      return result;
+    }
+
+    [HttpPost]
+    [MapToApiVersion("1.0")]
+    public async Task<CourseDTO> InputCourse()
+    {
+      CourseDTO example = new CourseDTO()
+      {
+        Id = "123",
+        Instructors = new List<string>() { "Otávio Olegário de Castro" },
+        Rating = 5,
+        Title = "Implementing MongoDB"
+      };
+
+      var result = await _courseService.CreateCourse(example);
       return result;
     }
   }
--- backend/src/Totvs.Olympus.API/Startup.cs
+++ backend/src/Totvs.Olympus.API/Startup.cs
@@ -38,6 +38,7 @@ namespace Totvs.Olympus.API
       });
       services.AddAutoSwaggerSetup(this.Configuration);
       services.SetupAluraService(this.Configuration);
+      services.SetupMongoDatabase(this.Configuration);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
