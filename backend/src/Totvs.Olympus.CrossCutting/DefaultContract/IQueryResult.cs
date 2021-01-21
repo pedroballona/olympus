69f6ed2d436110cf218ff42385a8bbbbad1bf9dc
Cristóvão <cristovaoolegario@gmail.com>
2021-01-21 12:46:26 -0300

    Adição das classes para paginação.

---
+++ backend/src/Totvs.Olympus.CrossCutting/DefaultContract/IQueryResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Totvs.Olympus.CrossCutting.DefaultContract
+{
+  public interface IQueryResult<out TDocument>
+  {
+    bool HasNext { get; }
+    IReadOnlyList<TDocument> Items { get; }
+  }
+}
---
+++ backend/src/Totvs.Olympus.CrossCutting/DefaultContract/RequestAllOptionsDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Totvs.Olympus.CrossCutting.DefaultContract
+{
+  [ExcludeFromCodeCoverage]
+  public class RequestAllOptionsDTO
+  {
+    [FromQuery(Name = "order")]
+    public string Order { get; set; }
+
+    [FromQuery(Name = "page")]
+    public int? Page { get; set; }
+
+    [FromQuery(Name = "pageSize")]
+    public int? PageSize { get; set; }
+  }
+}
