8b43220bf27fe2138e95393bee59cc544f1f0381
Cristóvão <cristovaoolegario@gmail.com>
2021-01-21 11:15:49 -0300

    Adição do serviço de comunicação com a Alura.

---
+++ backend/src/Totvs.Olympus.Domain/ExternalServices/IAluraHttpService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Totvs.Olympus.Domain.ExternalServices
+{
+  public interface IAluraHttpService
+  {
+    Task<ResponseDto> SendRequest<RequestDto, ResponseDto>(HttpMethod method, string url, RequestDto content);
+  }
+}
