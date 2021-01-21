a72a7f263e48523520950be95429713a08f3ab08
Cristóvão <cristovaoolegario@gmail.com>
2021-01-21 16:47:10 -0300

    Adição de TU do serviço http do alura.

---
+++ backend/src/Totvs.Olympus.Infrastructure.UnitTests/UnitTestHelpers/BasicCourseDataDTOTestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Totvs.Olympus.CrossCutting.ExternalServices.ResponsesDTO;
+
+namespace Totvs.Olympus.Infrastructure.UnitTests.UnitTestHelpers
+{
+  public static class BasicCourseDataDTOTestHelper
+  {
+    public static BasicCourseDataDTO GetBasicCourseDataDTO()
+    {
+      return new BasicCourseDataDTO()
+      {
+        Nome = ".Net e MongoDB parte 1: Primeiros passos para usar esse famoso banco NoSQL",
+        Slug = "dotnet-mongodb",
+        TempoEstimado = 16
+      };
+    } 
+  }
+}
