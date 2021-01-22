a006fd508c17273a19a5b4e8686c17a1d47243be
Pedro Ballona <45241329+pedroballona@users.noreply.github.com>
2021-01-22 12:58:13 -0300

    Course page (#23)
    * Modificado busca de cursos
    
    * Pagina de lição
    
    * Commit na main
    
    * Teste unitário
    
    Co-authored-by: Pedro Ballona <pedro.ballona@totvs.com.br>

--- frontend/src/app/pages/courses-page/components/course-detail-modal/course-detail-modal.component.spec.ts
+++ frontend/src/app/pages/courses-page/components/course-detail-modal/course-detail-modal.component.spec.ts
@@ -1,20 +1,62 @@
+import { ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable, of } from 'rxjs';
+import { PoModalComponent } from '@po-ui/ng-components';
+import { of } from 'rxjs';
+import { Course } from '../../../../models/course.model';
 import { CourseDetailModalComponent } from './course-detail-modal.component';
 
-
-class TranslateServiceMock {
-  get(key: string): Observable<string> {
-    return of(key);
-  }
-}
+const course: Course = {
+  title: 'Title',
+  description: 'Description',
+  firstClass: 'First Class',
+  instructors: [
+    'instructor 1',
+    'instructor 2'
+  ],
+  score: 4
+};
 
 describe('CourseDetailModalComponent', () => {
   let component: CourseDetailModalComponent;
   let translateService: jasmine.SpyObj<TranslateService>;
+  let modal: jasmine.SpyObj<PoModalComponent>;
+  let changeDetectorRef: jasmine.SpyObj<ChangeDetectorRef>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    translateService = jasmine.createSpyObj('teste', ['get']);
-    translateService.get.and.callFake(key => of(key));
+    translateService = jasmine.createSpyObj('translate', ['get']);
+    translateService.get.and.callFake((key) => of(key));
+    modal = jasmine.createSpyObj('modal', ['open', 'close']);
+    router = jasmine.createSpyObj('router', ['navigate']);
+    changeDetectorRef = jasmine.createSpyObj('changeDetectorRef', ['detectChanges']);
+
+    component = new CourseDetailModalComponent(
+      changeDetectorRef,
+      translateService,
+      router
+    );
+    component.modal = modal;
+  });
+
+  it('open', () => {
+    component.open('1', course);
+    expect(component.course).toEqual(course);
+    expect(component.courseId).toEqual('1');
+    expect(modal.open).toHaveBeenCalled();
+  });
+
+  it('close', () => {
+    component.course = course;
+    component.courseId = '1';
+    component.close();
+    expect(modal.close).toHaveBeenCalled();
+    expect(component.course).toBeNull();
+    expect(component.courseId).toBeNull();
+  });
+
+  it('goToLesson', () => {
+    component.goToLesson('1');
+    expect(router.navigate).toHaveBeenCalledWith(['lesson', '1']);
   });
 });
