42219ce4038400792548613a40eb0d7d36ac27c5
Cristóvão <cristovaoolegario@gmail.com>
2021-01-22 12:12:21 -0300

    Adicionando o endpoint de lookup de cursos.

--- backend/src/Totvs.Olympus.Domain/RepositoryContracts/ICourseRepository.cs
+++ backend/src/Totvs.Olympus.Domain/RepositoryContracts/ICourseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Totvs.Olympus.CrossCutting.DefaultContract;
 using Totvs.Olympus.CrossCutting.DTOs;
@@ -12,5 +13,6 @@ namespace Totvs.Olympus.Domain.RepositoryContracts
     Task<IQueryResult<CourseDTO>> GetAllPaginatedCourses(string filter, RequestAllOptionsDTO optionsDTO);
     Task<DetailCourseDTO> GetCourseById(Guid id);
     Task<Course> GetCourseByExternalId(string externalId);
+    Task<List<CourseDTO>> GetLookupCourses();
   }
 }
